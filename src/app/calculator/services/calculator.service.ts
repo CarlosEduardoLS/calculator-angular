5f580b626f06c0fa238c9a87f33453e33a905a69
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-12 16:33:11 -0300

    FEAT: Created calculate function

--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -4,5 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CalculatorService {
+  static readonly addition = '+';
+  static readonly substraction = '-';
+  static readonly division = '/';
+  static readonly multiplication = '*';
+
   constructor() {}
+
+  calculate(num1: number, num2: number, operation: string): number {
+    let result: number;
+    switch (operation) {
+      case CalculatorService.addition:
+        result = num1 + num2;
+        break;
+      case CalculatorService.substraction:
+        result = num1 - num2;
+        break;
+      case CalculatorService.division:
+        result = num1 / num2;
+        break;
+      case CalculatorService.multiplication:
+        result = num1 * num2;
+        break;
+      default:
+        result = 0;
+        break;
+    }
+    return result;
+  }
 }
