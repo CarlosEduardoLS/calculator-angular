b8b927fc685f1335d0fe065356d9c44b6ff3b389
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2021-09-12 16:50:12 -0300

    FIX: Insert calculator module to dependecy test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
+import { CalculatorModule } from './calculator/calculator.module';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [CalculatorModule],
     }).compileComponents();
   });
 
